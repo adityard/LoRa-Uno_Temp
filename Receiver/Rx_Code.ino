bfc43cdd3fb81f8e08246547b653c3a85cfa2185
adityard <62769136+adityard@users.noreply.github.com>
2020-03-31 01:52:12 +0530

    Receiver Arduino Code

---
+++ Receiver/Rx_Code.ino
@@ -0,0 +1,26 @@
+#include <SPI.h>
+#include <LoRa.h>
+void setup() 
+{
+  Serial.begin(9600);
+  Serial.println("LoRa Receiver");
+  if (!LoRa.begin(433E6)) 
+  {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+}
+void loop() 
+{
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) 
+  {
+    Serial.print("Received packet '");
+    while (LoRa.available()) 
+    {
+      Serial.print((char)LoRa.read());
+    }
+    Serial.print("' with RSSI ");
+    Serial.println(LoRa.packetRssi());
+  }
+}
